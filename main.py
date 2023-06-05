f2d8cc847605035e7c7137d18b20c5cf009439d1
Elisha <ukaigve1990@mail.ru>
2023-06-05 20:48:33 +0300

    add . 26 28

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-a = 7
-a %= 3
-print(a)
+# print(round(5 / 3)
+# print(min(5, 4, 2, -3, -65, 3))
+# print(max(5, 4, 2, -3, -65, 3))
+# print(pow(3, 5))
+# print(abs(-5))
+a = 2
+b = 3
+print(pow(a, b))
